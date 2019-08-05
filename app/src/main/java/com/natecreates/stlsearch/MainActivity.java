572ce793071bb5ab29f8faba9ac18f2f16d7adc3
Nathan <nnelson9@asu.edu>
2019-08-04 22:47:34 -0700

    Manifest updated, search result activity.java added

--- app/src/main/java/com/natecreates/stlsearch/MainActivity.java
+++ app/src/main/java/com/natecreates/stlsearch/MainActivity.java
@@ -2,12 +2,63 @@ package com.natecreates.stlsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText searchInput;
+    private Button searchButton;
+    private String searchStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchInput = findViewById(R.id.SearchBox);
+        searchButton = findViewById(R.id.SearchButton);
+
+
     }
+    public void search() throws IOException {
+        //take string from searchBox
+        //download webpage from yeggi
+        //parse html and display in form of card view
+        searchStr = searchInput.getText().toString();
+        //string of website
+        String html = "";
+        //url to be searched
+        String url = "https://www.yeggi.com/q/"+searchStr+"/";
+
+
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
+        HttpResponse response = client.execute(request);
+        InputStream in = response.getEntity().getContent();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder str = new StringBuilder();
+        String line = null;
+        while((line = reader.readLine()) != null)
+        {
+            str.append(line);
+        }
+        in.close();
+        html = str.toString();
+
+
+
+
+    }
+
 }
